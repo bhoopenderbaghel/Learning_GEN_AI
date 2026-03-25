baa7fb276ad50fb93aae97fbb81748bf1106a203
bhoopender <bhoopenderbaghel888@gmail.com>
2026-03-25 17:04:31 +0530

    added straming functionality in UI

--- Langgraph/workflows/streamlit_front.py
+++ Langgraph/workflows/streamlit_front.py
@@ -20,14 +20,22 @@ if user_input:
     st.session_state['message_history'] .append({'role': 'user', 'content': user_input})
     with st.chat_message('user'):
         st.text(user_input)
-    try:
+    # try:
 
-        response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]},config=CONFIG)
-        ai_message = response['messages'][-1].content
-    except Exception as e:
-        st.error(str(e))
-        st.stop()
-    st.session_state['message_history'] .append({'role':'assistant', 'content': ai_message})
+        # response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]},config=CONFIG)
+        # ai_message = response['messages'][-1].content
+    # except Exception as e:
+    #     st.error(str(e))
+    #     st.stop()
+     # st.session_state['message_history'] .append({'role':'assistant', 'content': ai_message})
     
     with st.chat_message('assistant'):
-        st.text(ai_message)
+        # st.text(ai_message)
+        ai_message = st.write_stream(
+            message_chunk.content for message_chunk, metadata in chatbot.stream(
+            {'messages': [HumanMessage(content= user_input)]},
+            config= {'configurable':{'thread_id':'thread-1'}},
+            stream_mode= 'messages'
+        )
+        )
+    st.session_state['message_history'] .append({'role':'assistant', 'content': ai_message})  
