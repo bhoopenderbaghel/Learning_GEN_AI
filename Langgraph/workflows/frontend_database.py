d3b8480765ed49326fc48663945f558016e0cbf4
bhoopender <bhoopenderbaghel888@gmail.com>
2026-05-20 15:00:21 +0530

    Added the observability in Chat Bot

--- Langgraph/workflows/frontend_database.py
+++ Langgraph/workflows/frontend_database.py
@@ -70,13 +70,21 @@ if user_input:
     with st.chat_message('user'):
         st.text(user_input)
 
+    CONFIG = {
+        'configurable': {
+            'thread_id': st.session_state['thread_id']
+        },
+        "metadata":{"thread_id": st.session_state['thread_id']},
+        "run_name": "chat_node",
+    }
         
     with st.chat_message('assistant'):
         # st.text(ai_message)
         ai_message = st.write_stream(
         (message_chunk.content for message_chunk, metadata in chatbot.stream(
         {'messages': [HumanMessage(content=user_input)]},
-        config={'configurable': {'thread_id': st.session_state['thread_id']}},
+        # config={'configurable': {'thread_id': st.session_state['thread_id']}},
+        config = CONFIG,
         stream_mode='messages'
     ))
         )
